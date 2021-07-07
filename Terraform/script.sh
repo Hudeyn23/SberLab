94f13089514f53702123804c4522c5612f49fa80
Borodun <a.borodin2@g.nsu.ru>
2021-07-07 20:21:24 +0700

    Terraform remote code execution

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -3,7 +3,8 @@
 apt update
 apt ugrade -y
 apt install -y golang
-git clone https://github.com/borodun/SberLab.git
-cd SberLab/Server/ || exit
+mkdir /root/SberLab
+git clone https://github.com/borodun/SberLab.git /root/SberLab/
+cd /root/SberLab/Server/ || exit
 go build
 ./Server
